0948a3e05345b993d8ded6909b8469ef8bb72feb
Hugo Salles <salles.hugo05@gmail.com>
2022-08-09 15:39:52 -0400

    adding more forms and validations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [title, setTitle] = useState("This is the title");
+  const [description, setDescription] = useState("Your description");
+  const [author, setAuthor] = useState("Todd Albert");
+
+  // console.log(title)'
+
+  // const formSubmit = async (e) => {}
+
+  async function formSubmit(e) {
+    try {
+      e.preventDefault();
+      // console.log("form submitted")
+
+      // const comment = {
+      //   title:title,
+      //   description:description,
+      //   author:author,
+      // }
+      const comment = {
+        title,
+        description,
+        author,
+      };
+
+      console.log("form submitted with", comment);
+
+      // really submit it to an api
+      const results = await fetch("https://sql.bocacode.com/comments", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(comment),
+      });
+      console.log(results);
+      const data = await results.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form onSubmit={formSubmit}>
+        <h1>Comments</h1>
+
+        {/* here goes the title */}
+        <label>Title</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <h3>{title}</h3>
+
+        {/* this is the description */}
+        <label>Description</label>
+        <textarea
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        ></textarea>
+        <h3>{description}</h3>
+
+        {/* THIS IS THE AUTHOR */}
+
+        <label>Author</label>
+        <select
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="" selected>
+            Choose One
+          </option>
+          <option value="Todd">Todd</option>
+          <option value="Damian">Damian</option>
+          <option value="Other">Other</option>
+        </select>
+        <h3>{author}</h3>
+        <button>Submit Form</button>
+      </form>
     </div>
   );
 }
